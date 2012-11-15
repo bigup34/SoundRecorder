db212dfbaea2b54272922bc26d451f9f3fc70224
Anthony Reboul <anthony.reboul@epitech.eu>
2012-11-15 17:44:11 +0800

    UI Improvments

--- SoundRecorder/gen/com/soundrecorder/activities/R.java
+++ SoundRecorder/gen/com/soundrecorder/activities/R.java
@@ -71,23 +71,24 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int application_image_description=0x7f040004;
-        public static final int application_version=0x7f040007;
-        public static final int back_image=0x7f04000c;
-        public static final int background_color=0x7f04000a;
-        public static final int converter_button_text=0x7f040006;
-        public static final int hello_world=0x7f04000e;
+        public static final int application_version=0x7f040006;
+        public static final int back_image=0x7f04000b;
+        public static final int background_color=0x7f040009;
         public static final int manager_button_text=0x7f040005;
         public static final int menu_settings=0x7f040001;
-        public static final int micro_image=0x7f04000b;
+        public static final int micro_image=0x7f04000a;
+        public static final int no=0x7f04000e;
+        public static final int popup_txt=0x7f04000f;
         public static final int record_button_text=0x7f040003;
-        public static final int sound_player_button_text=0x7f040009;
-        public static final int time=0x7f04000d;
-        public static final int title_activity_file_manager_view=0x7f04000f;
-        public static final int title_activity_pop_up=0x7f040012;
-        public static final int title_activity_record_view=0x7f040008;
+        public static final int sound_player_button_text=0x7f040008;
+        public static final int time=0x7f04000c;
+        public static final int title_activity_file_manager_view=0x7f040010;
+        public static final int title_activity_pop_up=0x7f040013;
+        public static final int title_activity_record_view=0x7f040007;
         public static final int title_activity_recorder_menu=0x7f040002;
-        public static final int title_activity_settings_view=0x7f040010;
-        public static final int title_activity_sound_player_view=0x7f040011;
+        public static final int title_activity_settings_view=0x7f040011;
+        public static final int title_activity_sound_player_view=0x7f040012;
+        public static final int yes=0x7f04000d;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
